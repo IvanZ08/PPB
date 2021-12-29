0876144290cf4dc03d0a42cd13d052af9ceb67dc
IvanZ08 <ivan.stepheng@gmail.com>
2021-12-29 15:31:12 +0700

    first commit

---
+++ app/src/main/java/com/example/projectfirebase/FormPegawai.java
@@ -0,0 +1,40 @@
+package com.example.projectfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class FormPegawai extends AppCompatActivity {
+
+    EditText xnopeg,xnmapeg,xjabatan;
+    Button tblSimpan;
+    DatabaseReference dbref = FirebaseDatabase.getInstance().getReference().child("Absen");
+    Pegawai pegawai = new Pegawai();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_pegawai);
+
+        xnopeg=findViewById(R.id.no_pegawai);
+        xnmapeg=findViewById(R.id.nama_pegawai);
+        xjabatan=findViewById(R.id.jabatan);
+        tblSimpan=findViewById(R.id.tombolsimpan);
+
+        tblSimpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pegawai.setNomor_peg(xnopeg.getText().toString().trim());
+                pegawai.setNama_peg(xnmapeg.getText().toString().trim());
+                pegawai.setJataban(xjabatan.getText().toString().trim());
+
+                dbref.push().setValue(pegawai);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/projectfirebase/Pegawai.java
@@ -0,0 +1,31 @@
+package com.example.projectfirebase;
+
+public class Pegawai {
+    private String nomor_peg;
+    private String nama_peg;
+    private String jataban;
+
+    public String getNomor_peg() {
+        return nomor_peg;
+    }
+
+    public void setNomor_peg(String nomor_peg) {
+        this.nomor_peg = nomor_peg;
+    }
+
+    public String getNama_peg() {
+        return nama_peg;
+    }
+
+    public void setNama_peg(String nama_peg) {
+        this.nama_peg = nama_peg;
+    }
+
+    public String getJataban() {
+        return jataban;
+    }
+
+    public void setJataban(String jataban) {
+        this.jataban = jataban;
+    }
+}
